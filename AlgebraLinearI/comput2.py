2fb9ab33b5f035e924ee0f2763fe58efc6ba3e99
Csaba Schneider <csaba.schneider@gmail.com>
2025-03-29 14:48:39 -0300

    tried to add diagram

--- AlgebraLinearI/comput2.py
+++ AlgebraLinearI/comput2.py
@@ -23,3 +23,18 @@ def adjacency_matrix( n, edge_list ):
         mat[e[0],e[1]] = mat[e[1],e[0]] = 1
         
     return mat 
+
+
+
+def sum_embed( mylist ):
+
+    len_mylist = len( mylist )
+    if len_mylist == 1 and not isinstance( mylist[0], list ):
+        return mylist[0]
+    elif len_mylist == 1 and isinstance( mylist[0], list ):
+        return sum_embed( mylist[0] )
+    else: 
+        mid = len_mylist//2
+        return sum_embed( mylist[:mid] ) + sum_embed( mylist[mid:] )
+
+print( sum_embed( [[1],[2,[3,4]],[[10,11]]] ))
